03388aa086852eebcd6a5d63066bce1de29ceb4a
Mathias Tausen <mta@gomspace.com>
2016-12-06 10:52:16 +0100

    xmobar date and weather format

--- xmobar.hs
+++ xmobar.hs
@@ -11,12 +11,12 @@ Config {
     lowerOnStart = True,
     overrideRedirect = False,
     commands = [
-        Run Weather "KPAO" ["-t","<tempF>F <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
+        Run Weather "KPAO" ["-t","<tempC>C <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
         Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Network "em1" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Date "%a %b %_d %l:%M" "date" 10,
+        Run Date "%a %b %_d %H:%M" "date" 10,
 	Run BatteryP ["BAT0", "BAT1"]
 	             ["-t", "<acstatus>"
 		     , "-L", "10", "-H", "80"
